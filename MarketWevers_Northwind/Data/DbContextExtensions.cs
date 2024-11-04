54d3fa6d730b971ad9cf1241b87bf73d93d05191
LAISHAWJ <lw2021-0468@uce.edu.do>
2024-11-04 02:11:13 -0400

    Scaffolding models and data

--- MarketWevers_Northwind/Data/DbContextExtensions.cs
+++ MarketWevers_Northwind/Data/DbContextExtensions.cs
@@ -9,7 +9,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace MarketWevers_Northwind.DATA
+namespace MarketWevers_Northwind.Data
 {
     public static class DbContextExtensions
     {
